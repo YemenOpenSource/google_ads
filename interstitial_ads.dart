62d037cc23325e2dc7dea15e98009547499efba3
Amjad Alhitary <83473041+Amjedgit@users.noreply.github.com>
2022-09-17 13:42:32 +0300

    Update interstitial_ads.dart

--- interstitial_ads.dart
+++ interstitial_ads.dart
@@ -1,74 +1,74 @@
-//
-// import 'package:google_mobile_ads/google_mobile_ads.dart';
-//
-// import 'ads.dart';
-//
-// class AdInterstitial{
-//   AdInterstitial._();
-//   static AdInterstitial instance=AdInterstitial._();
-//   InterstitialAd? _interstitialAd;
-//    int maxFailedLoadAttempts = 3;
-//   int _numInterstitialLoadAttempts = 0;
-//
-// // void loadIntersitialAd(){
-// //   InterstitialAd.load(
-// //       adUnitId: AdsId.interstitialAdUnitId,
-// //       request: const AdRequest(),
-// //       adLoadCallback: InterstitialAdLoadCallback(
-// //         onAdLoaded: (InterstitialAd ad) {
-// //           _interstitialAd = ad;
-// //           _showInterstitialAd();
-// //         },
-// //         onAdFailedToLoad: (LoadAdError error) {
-// //           print('InterstitialAd failed to load: $error');
-// //         },
-// //       ));
-// // }
-//   void createInterstitialAd() {
-//     InterstitialAd.load(
-//         adUnitId: AdsId.interstitialAdUnitId,
-//         request: const AdRequest(),
-//         adLoadCallback: InterstitialAdLoadCallback(
-//           onAdLoaded: (InterstitialAd ad) {
-//             _interstitialAd = ad;
-//             _numInterstitialLoadAttempts = 0;
-//             _interstitialAd!.setImmersiveMode(true);
-//             _showInterstitialAd();
-//           },
-//           onAdFailedToLoad: (LoadAdError error) {
-//             print('InterstitialAd failed to load: $error.');
-//             _numInterstitialLoadAttempts += 1;
-//             _interstitialAd = null;
-//             if (_numInterstitialLoadAttempts < maxFailedLoadAttempts) {
-//               createInterstitialAd();
-//             }
-//           },
-//         ));
-//   }
-//   void _showInterstitialAd() {
-//     if (_interstitialAd == null) {
-//       print('Warning: attempt to show interstitial before loaded.');
-//       return;
-//     }
-//     _interstitialAd?.fullScreenContentCallback = FullScreenContentCallback(
-//       onAdShowedFullScreenContent: (InterstitialAd ad) =>
-//           print('ad onAdShowedFullScreenContent.'),
-//       onAdDismissedFullScreenContent: (InterstitialAd ad) {
-//         print('$ad onAdDismissedFullScreenContent.');
-//         ad.dispose();
-//         createInterstitialAd();
-//       },
-//       onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
-//         print('$ad onAdFailedToShowFullScreenContent: $error');
-//         ad.dispose();
-//         createInterstitialAd();
-//       },
-//     );
-//     _interstitialAd?.show();
-//     _interstitialAd = null;
-//   }
-//
-//   void dispose() {
-//     _interstitialAd?.dispose();
-//   }
+
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import 'ads.dart';
+
+class AdInterstitial{
+  AdInterstitial._();
+  static AdInterstitial instance=AdInterstitial._();
+  InterstitialAd? _interstitialAd;
+   int maxFailedLoadAttempts = 3;
+  int _numInterstitialLoadAttempts = 0;
+
+// void loadIntersitialAd(){
+//   InterstitialAd.load(
+//       adUnitId: AdsId.interstitialAdUnitId,
+//       request: const AdRequest(),
+//       adLoadCallback: InterstitialAdLoadCallback(
+//         onAdLoaded: (InterstitialAd ad) {
+//           _interstitialAd = ad;
+//           _showInterstitialAd();
+//         },
+//         onAdFailedToLoad: (LoadAdError error) {
+//           print('InterstitialAd failed to load: $error');
+//         },
+//       ));
 // }
+  void createInterstitialAd() {
+    InterstitialAd.load(
+        adUnitId: AdsId.interstitialAdUnitId,
+        request: const AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+            _interstitialAd = ad;
+            _numInterstitialLoadAttempts = 0;
+            _interstitialAd!.setImmersiveMode(true);
+            _showInterstitialAd();
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error.');
+            _numInterstitialLoadAttempts += 1;
+            _interstitialAd = null;
+            if (_numInterstitialLoadAttempts < maxFailedLoadAttempts) {
+              createInterstitialAd();
+            }
+          },
+        ));
+  }
+  void _showInterstitialAd() {
+    if (_interstitialAd == null) {
+      print('Warning: attempt to show interstitial before loaded.');
+      return;
+    }
+    _interstitialAd?.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (InterstitialAd ad) =>
+          print('ad onAdShowedFullScreenContent.'),
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        print('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+        createInterstitialAd();
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        createInterstitialAd();
+      },
+    );
+    _interstitialAd?.show();
+    _interstitialAd = null;
+  }
+
+  void dispose() {
+    _interstitialAd?.dispose();
+  }
+}
